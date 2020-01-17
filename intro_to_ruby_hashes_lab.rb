bddfc2ef13ea623a268dc6b94382346fae458f69
Gary Alberto Cordero Rosa <garycordero1690@gmail.com>
2020-01-17 22:19:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,7 @@ def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method'
   my_hash = Hash.new
   my_hash[key] = value
+  puts my_hash
 end
 
 def read_from_hash(hash, key)
